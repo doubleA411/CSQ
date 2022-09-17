55b44587a4f9ecb2d3fcfb4bbd16076666e1b01c
Aakash <56353087+xii-aakash@users.noreply.github.com>
2022-09-17 20:49:09 +0530

    17/09-4

--- scroll.js
+++ scroll.js
@@ -1,5 +1,11 @@
+function show() {
+    // check =  document.getElementsByClassName('.check');
+    const cl = document.getElementById("check2");
 
-function scrollWin() {
-    window.scrollTo(0, 99999);
+    if (cl.className == "fa-solid fa-x") {
+        cl.className = "fas fa-bars";
+    }
+    else {
+        cl.className = "fa-solid fa-x"
+    }
 }
-
