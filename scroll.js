f9596156087f858afed568d6277dd4d544ab038d
S-15-77 <santhoshdayakar2002@gmail.com>
2022-09-28 21:27:19 +0530

    28/9

--- scroll.js
+++ scroll.js
@@ -9,3 +9,23 @@ function show() {
         cl.className = "fa-solid fa-x"
     }
 }
+
+
+
+
+      // Set things up
+      window.addEventListener("load", () => {
+        let observer = new IntersectionObserver((entries) => { 
+          entries
+            .forEach((e) => {
+              if (e.target) {
+                e.target.classList.toggle("in-view", e.isIntersecting);
+              }
+            })
+        });
+        
+        const elements = document.getElementsByClassName("hscroll-line-b")
+        for (let i = 0; i < elements.length; i++) {
+          observer.observe(elements[i]);  
+        }
+      });
